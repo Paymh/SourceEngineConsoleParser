475dc78055313eb330b875aeb9996523397fc4b0
Paymh <lachlan.horsey@gmail.com>
2015-05-18 22:38:13 +1000

    Added Auto-spectate extension for console parser

---
+++ SourceEngineConsoleParser/Extensions/AutoSpec.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SourceEngineConsoleParser
+{
+    //Auto spectate extension for console parser
+
+    public class AutoSpec : Parser
+    {
+        bool isEnabled = false;
+        public void Parse(string text)
+        {
+            if (text.Contains("parser_autospec_enabled"))
+            {
+                isEnabled = true;
+                Program.logger.WriteLine("Auto-spectate Enabled", Logger.LogLevel.Warn);
+                Program.ExecuteIngame("echo Parser > Auto-spectate enabled");
+                return;
+            }
+            else if (text.Contains("parser_autospec_disabled"))
+            {
+                isEnabled = false;
+                Program.logger.WriteLine("Auto-spectate Disabled", Logger.LogLevel.Warn);
+                Program.ExecuteIngame("echo Parser > Auto-spectate disabled");
+                return;
+            }
+            else if (isEnabled && text.ToLowerInvariant().Contains("spec") && !text.Contains("Auto-spectate"))
+            {
+                Program.ExecuteIngame("spectate");
+                Program.logger.WriteLine("Going spectate", Logger.LogLevel.Info);
+            }
+        }
+    }
+}
