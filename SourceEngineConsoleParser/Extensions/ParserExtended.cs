8f8e37b3c8e1e1387a9b55eb6af57409e9181f5c
Paymh <lachlan.horsey@gmail.com>
2015-05-18 22:07:51 +1000

    Two example extensions that show the basics of what is possible

---
+++ SourceEngineConsoleParser/Extensions/ParserExtended.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SourceEngineConsoleParser
+{
+    //This class is compiled at runtime to allow easy additions / changes to be made without the need to recompile the main program
+    public class ParserExtended : Parser
+    {
+        public void Parse(string text)
+        {
+            //Change logic below
+            Console.WriteLine("Extended Parse: " + text);
+        }
+    }
+}
---
+++ SourceEngineConsoleParser/Extensions/SmileyBinder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SourceEngineConsoleParser
+{
+    //This class is compiled at runtime to allow easy additions / changes to be made without the need to recompile the main program
+    public class SmileyBinder : Parser
+    {
+        bool enabled = false;
+        string playerName = "";
+        public void Parse(string text)
+        {
+            //Change logic below
+            if (text.Contains("parser_shittalk_enabled"))
+            {
+                Program.logger.WriteLine("Smiley Binding enabled",Logger.LogLevel.Warn);
+                enabled = true;
+                Program.ExecuteIngame(new String[] { "name", "echo Parser > Shittalk enabled" });
+
+            }
+            else if (text.Contains("parser_shittalk_disabled"))
+            {
+                Program.logger.WriteLine("Smiley Binding disabled", Logger.LogLevel.Warn);
+                Program.ExecuteIngame("echo Parser > Shittalk disabled");
+                enabled = false;
+            }
+            if (text.Contains("\"name\" = \""))
+            {
+                String[] temp = text.Split('\"');
+                playerName = temp[3];
+            }
+            if (enabled && text.Contains(playerName + " killed"))
+            {
+                Program.ExecuteIngame("say =)");
+                Program.logger.WriteLine("Smashed an idiot (Sent shittalk)",Logger.LogLevel.Success);
+            }
+        }
+    }
+}
