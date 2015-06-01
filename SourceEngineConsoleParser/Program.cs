8e88e306e21c5cb33e1f6c25275ff49d11b14666
Paymh <lachlan.horsey@gmail.com>
2015-06-01 18:55:50 +1000

    Reverted changes to config pathing to fix more errors caused by lasted pull request

--- SourceEngineConsoleParser/Program.cs
+++ SourceEngineConsoleParser/Program.cs
@@ -46,7 +46,7 @@ namespace SourceEngineConsoleParser
             }
             ReadConsole();
         }
-
+        
         /// <summary>
         /// Reads the entire log file, then reads new entries to the log file every 10 ms after being called
         /// Creates the out.log file if it does not exist already
@@ -105,7 +105,7 @@ namespace SourceEngineConsoleParser
                 if (gameDir.Length > 0 && gameDir[gameDir.Length - 1] != '\\' && gameDir[gameDir.Length - 1] != '/')
                     gameDir += "\\";
                 logger.WriteLine("", Logger.LogLevel.Nothing);
-                logger.Write("Please enter the subdirectory to the custom folder containing your autoexec. (eg custom\\customstuff\\) :  ", Logger.LogLevel.Info);
+                logger.Write("Please enter the subdirectory to the custom folder containing your autoexec. (eg C:\\Program Files (x86)\\Steam\\SteamApps\\common\\Team Fortress 2\\tf\\custom\\customstuff\\) :  ", Logger.LogLevel.Info);
                 customPath = Console.ReadLine();
                 if (customPath.Length > 0 && customPath[customPath.Length - 1] != '\\' && customPath[customPath.Length - 1] != '/')
                     customPath += "\\";
@@ -133,15 +133,7 @@ namespace SourceEngineConsoleParser
                 gameDir = sr.ReadLine();
                 customPath = sr.ReadLine();
                 keyValue = sr.ReadLine();
-                sr.Close();
-                // Handle old format custom folder paths
-                if (customPath.Contains(gameDir.Remove(gameDir.Length - 1)))
-                {
-                    logger.Write("Your config file is in the old format. Press any key to re-run setup.",Logger.LogLevel.Warn);
-                    Console.ReadKey();
-                    File.Delete("config.cfg");
-                    ReadConfig();
-                }
+                sr.Close();                
             }
         }
 
@@ -152,7 +144,7 @@ namespace SourceEngineConsoleParser
         public static void ExecuteIngame(String[] commands)
         {
             //Write commands to config file
-            File.WriteAllLines(gameDir + customPath + @"cfg\executer.cfg", commands);
+            File.WriteAllLines(customPath + @"cfg\executer.cfg", commands);
             //Press bind to execute config
             SendKeyPress();
         }
@@ -164,7 +156,7 @@ namespace SourceEngineConsoleParser
         public static void ExecuteIngame(String command)
         {
             //Write commands to config file
-            File.WriteAllText(gameDir + customPath + @"cfg\executer.cfg", command);
+            File.WriteAllText(customPath + @"cfg\executer.cfg", command);
             //Press bind to execute config
             SendKeyPress();
         }
@@ -191,19 +183,19 @@ namespace SourceEngineConsoleParser
             StreamWriter sw = null;
             try
             {
-                autoexec = File.ReadAllText(gameDir + customPath + @"cfg\autoexec.cfg");
-                sw = new StreamWriter(gameDir + customPath + @"cfg\autoexec.cfg", true);
+                autoexec = File.ReadAllText(customPath + @"cfg\autoexec.cfg");
+                sw = new StreamWriter(customPath + @"cfg\autoexec.cfg", true);
             }
             catch (System.IO.DirectoryNotFoundException)
             {
-                logger.Write("Invalid path to custom directory:\n" + gameDir + customPath + "\nPress any key to exit.",Logger.LogLevel.Error);
+                logger.Write("Invalid path to custom directory:\n" + customPath + "\nPress any key to exit.",Logger.LogLevel.Error);
                 File.Delete("config.cfg");
                 Console.ReadKey();
                 Environment.Exit(0);
             }
             catch (System.NotSupportedException)
             {
-                logger.Write("Invalid path to custom directory:\n" + gameDir + customPath + "\nPress any key to exit.",Logger.LogLevel.Error);
+                logger.Write("Invalid path to custom directory:\n" + customPath + "\nPress any key to exit.",Logger.LogLevel.Error);
                 File.Delete("config.cfg");
                 Console.ReadKey();
                 Environment.Exit(0);
